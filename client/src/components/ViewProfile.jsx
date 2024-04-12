a1e3ca467e8602512ed3057996395a17f0f0185a
psg-19 <pratushgupt22@gmail.com>
2024-04-12 12:54:04 +0530

    fixed view profile page

--- client/src/components/ViewProfile.jsx
+++ client/src/components/ViewProfile.jsx
@@ -43,9 +43,9 @@ const navigate=useNavigate()
         </div>
        
 
-      
+     
 
-        <div className='flex flex-col items-center justify-center gap-y-4'>
+        <div className='flex flex-col  justify-center gap-y-4'>
     <p> <b> Name :</b> {user.firstName} {user.lastName}</p>
     <p><b>Email :</b> {user.email}</p>
        <p><b>Contact Number :</b> {user.phoneNo}</p>
@@ -53,6 +53,7 @@ const navigate=useNavigate()
     <p><b>District :</b> {user.district}</p>
     <p><b>Role : </b>{user.role}</p>
         </div>
+
 
 
 <button onClick={()=>setChange((prev)=> prev=prev+1)} className="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-green-400 to-blue-600 group-hover:from-green-400 group-hover:to-blue-600 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800">
