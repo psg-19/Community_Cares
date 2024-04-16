235175970723315e85750234108f7db280c6dd28
psg-19 <pratushgupt22@gmail.com>
2024-04-16 23:28:42 +0530

    trying to fix cors

--- server/index.jsx
+++ server/index.jsx
@@ -18,7 +18,7 @@ app.use(fileUpload(
     app.use(cookieParser());
 app.use(cors({
     // origin: 'http://localhost:3000',
-    origin:'*', 
+    origin: process.env.FRONTEND_URL, 
   optionsSuccessStatus: 200,
   credentials: true 
   }));
